8117a7df3a2d057ea03d60e699546fd4147e0a83
zhulin <zhulin_jay@163.com>
2017-03-01 18:35:56 +0800

    test code

--- service/phpproxy.lua
+++ service/phpproxy.lua
@@ -4,7 +4,7 @@ local dns = require "dns"
 
 local CMD = {}
 
-function CMD.test()
+local function request( ... )
 	httpc.dns()	-- set dns server
 	print("GET baidu.com")
 	local respheader = {}
@@ -22,6 +22,15 @@ function CMD.test()
 	print(string.format("GET %s (baidu.com)", ip))
 	local status, body = httpc.get("baidu.com", "/", respheader, { host = "baidu.com" })
 	print(status)
+end
+
+function CMD.test()
+	print("test")
+	skynet.fork(request)
+end
+
+function CMD.echo( text )
+	print(text)
 end
 
 skynet.start(function()
