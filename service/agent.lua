23235a20bdee0f19ed02f9e2fbcb5d9154aa16c0
zhulin <zhulin_jay@163.com>
2017-03-06 18:29:03 +0800

    agent

--- service/agent.lua
+++ service/agent.lua
@@ -4,6 +4,12 @@ local socket = require "socket"
 local CMD = {}
 local client_fd
 
+local HEADER_LEN		=	2+2+4+1+4
+
+-- 包头约定
+-- Mcmd, Scmd, BodyLen, encrypt, Context, PBData
+-- uint16,uint16,uint32,uint8,	uint32,	 bytes
+
 local function send_package(pack)
 	local package = string.pack(">s2", pack)
 	socket.write(client_fd, package)
@@ -14,12 +20,26 @@ local function make_header( mcmd, scmd, len )
 	return header
 end
 
+local function bin2hex(s)
+    s=string.gsub(s,"(.)",function (x) return string.format("%02X ",string.byte(x)) end)
+    return s
+end
+
+local function parse_header( data )
+	local mcmd, scmd, bodylen, encrypt, context = string.unpack(">HHI4BI4")
+	print(string.format("MCMD:%d, SCMD:%d, bodylen:%d, encrypt:%d, context:%d", mcmd, scmd, bodylen, encrypt, context))
+	return mcmd, scmd, bodylen, encrypt, context
+end
+
 function CMD.runloop( fd )
 	while(socket.block(fd)) do
-		local len = socket.read(fd, 4)
-		len = string.unpack(">I4", len)
-		local str = socket.read(fd, len)
-		print(string.format("[%d] Received: %s", fd, str))
+		local header = socket.read(fd, HEADER_LEN)
+		if not header then		--连接断开导致读到false
+			break
+		end
+		local mcmd, scmd, bodylen, encrypt, context = parse_header(header)
+		local pbdata = socket.read(fd, bodylen)
+		print(string.format("[%d] Received: %s", fd, bin2hex(pbdata)))
 	end
 	print("end while")
 	skynet.call("GATE", "lua", "close", fd)
